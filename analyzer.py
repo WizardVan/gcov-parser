493f66490002b55c58bb0e706c85e26a659c5ace
Ali Gholami <gholami@sics.se>
2014-11-15 01:16:15 +0100

    Make the output for each SF unique

--- analyzer.py
+++ analyzer.py
@@ -22,7 +22,7 @@ def main(argv):
          outputfile = arg   
 
    if os.path.isfile(inputfile):
-     infile = open(inputfile)
+     infile = open(inputfile, 'r')
      outfile = open(outputfile, 'w')
      kernel_usage(infile, outfile)
      infile.close()
@@ -36,20 +36,39 @@ This function generates the lines that are used used
 by each source file in the kernel
 """
 def kernel_usage(infile, outfile):
+    
+    tmp_path= ""
+
+    "A set of lines used in the SF"
+    da_values = set()    
+    usage_dict = {}    
 
     for lines in infile:
       if "SF:" in lines:
-        outfile.write(lines)
-
+        tmp_path= lines.rstrip(os.linesep)
+      
       if ("DA:" in lines) and ("FNDA" not in lines):
         lines = lines.replace("DA:", "")
         line = lines.split(",")
-        outfile.write(line[0]+'\n')
-
+        da_values.add(line[0])
+     
       if "end_of_record" in lines:
-        outfile.write('\n')   
+        tmp_set = usage_dict.get(tmp_path)        
+        if tmp_set is not None:
+          union = sorted(set().union(da_values,tmp_set))
+          usage_dict[tmp_path] = union
+        else:
+          usage_dict[tmp_path]= sorted(da_values)
+        da_values.clear()
 
+    for sf,da in usage_dict.items():
+      outfile.write(sf+'\n')
+      for lines in da:
+        outfile.write(lines+'\n')
+      outfile.write('\n')
+    
     pass
+
 
 if __name__ == "__main__":
    main(sys.argv[1:])
